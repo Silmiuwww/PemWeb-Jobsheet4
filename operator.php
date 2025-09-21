dca36a0f88574d2ef70695a91dc3406ea7e5d506
Silmiuww <nadiasalma078@gmail.com>
2025-09-21 17:29:55 +0700

    Praktikum bagian 3 soal no 3.2

--- operator.php
+++ operator.php
@@ -18,4 +18,21 @@ echo "Hasil Perkalian: {$hasilKali} <br>";
 echo "Hasil Pembagian: {$hasilBagi} <br>";
 echo "Sisa Pembagian: {$sisabagi} <br>";
 echo "Hasil Pemangkatan: {$pangkat} <br>";
+
+echo "<br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Apakah a == b? " . var_export($hasilSama, true) . "<br>";
+echo "Apakah a != b? " . var_export($hasilTidakSama, true) . "<br>";
+echo "Apakah a < b? " . var_export($hasilLebihKecil, true) . "<br>";
+echo "Apakah a > b? " . var_export($hasilLebihBesar, true) . "<br>";
+echo "Apakah a <= b? " . var_export($hasilLebihKecilSama, true) . "<br>";
+echo "Apakah a >= b? " . var_export($hasilLebihBesarSama, true) . "<br>";
+
 ?>
