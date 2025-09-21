d934f0e565c9d110245dacd00022c21cfdc2a4bd
Silmiuww <nadiasalma078@gmail.com>
2025-09-21 21:44:58 +0700

    Praktikum bagian 4 soal no 4.2

--- struktur_kontrol.php
+++ struktur_kontrol.php
@@ -10,4 +10,28 @@ if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
 } elseif ($nilaiNumerik < 70) {
     echo "Nilai huruf: D";
 } 
+
+echo "<br>";
+
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+echo "<h3>Perhitungan Target Jarak Atlet</h3>";
+echo "Jarak awal: $jarakSaatIni kilometer<br>";
+echo "Target jarak: $jarakTarget kilometer<br>";
+echo "Peningkatan jarak per hari: $peningkatanHarian kilometer<br><br>";
+
+echo "Proses perhitungan:<br>";
+while ($jarakSaatIni < $jarakTarget) {
+    $jarakSaatIni += $peningkatanHarian;
+    $hari++;
+    echo "Hari ke-$hari: Jarak mencapai $jarakSaatIni kilometer<br>";
+}
+
+echo "<br>";
+echo "Hasil: Atlet tersebut memerlukan $hari hari untuk mencapai jarak $jarakTarget kilometer";
+
+
 ?>
