361857dce7d7895c5b3aefee53a6eebc0b40498c
Silmiuww <nadiasalma078@gmail.com>
2025-09-21 17:12:35 +0700

    Praktikum bagian 3 soal no 3.1

---
+++ operator.php
@@ -0,0 +1,21 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasiltambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisabagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Nilai a: {$a} <br>";
+echo "Nilai b: {$b} <br><br>";
+
+echo "Hasil Penjumlahan: {$hasiltambah} <br>";
+echo "Hasil Pengurangan: {$hasilKurang} <br>";
+echo "Hasil Perkalian: {$hasilKali} <br>";
+echo "Hasil Pembagian: {$hasilBagi} <br>";
+echo "Sisa Pembagian: {$sisabagi} <br>";
+echo "Hasil Pemangkatan: {$pangkat} <br>";
+?>
