0b2764bf0e1ff4d4adec9fbe998b90c52a12fffd
Silmiuww <nadiasalma078@gmail.com>
2025-09-21 22:00:26 +0700

    Praktikum bagian 4 soal no 4.3

--- struktur_kontrol.php
+++ struktur_kontrol.php
@@ -11,14 +11,13 @@ if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
     echo "Nilai huruf: D";
 } 
 
-echo "<br>";
+echo "<br><br>";
 
 $jarakSaatIni = 0;
 $jarakTarget = 500;
 $peningkatanHarian = 30;
 $hari = 0;
 
-echo "<h3>Perhitungan Target Jarak Atlet</h3>";
 echo "Jarak awal: $jarakSaatIni kilometer<br>";
 echo "Target jarak: $jarakTarget kilometer<br>";
 echo "Peningkatan jarak per hari: $peningkatanHarian kilometer<br><br>";
@@ -33,5 +32,22 @@ while ($jarakSaatIni < $jarakTarget) {
 echo "<br>";
 echo "Hasil: Atlet tersebut memerlukan $hari hari untuk mencapai jarak $jarakTarget kilometer";
 
+echo "<br><br>";
 
+$jumlahLahan = 10;
+$jumlahPerlahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+echo "Jumlah lahan: $jumlahLahan<br>";
+echo "Jumlah tanaman per lahan: $jumlahPerlahan<br>";
+echo "Buah per tanaman: $buahPerTanaman<br><br>";
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($jumlahPerlahan * $buahPerTanaman);
+    echo "Lahan ke-$i: " . ($jumlahPerlahan * $buahPerTanaman) . " buah<br>";
+}
+
+echo "<br>";
+echo "Total jumlah buah yang akan dipanen adalah: $jumlahBuah buah";
 ?>
