0219c8739d507e8e807a1ebbfb0a8d2f8692967b
Silmiuww <nadiasalma078@gmail.com>
2025-09-22 22:37:26 +0700

    Praktikum bagian 5 soal no 5.3

--- array.php
+++ array.php
@@ -30,4 +30,32 @@ foreach ($daftarKaryawan as $karyawan) {
 }
 
 echo "Daftar karyawan dengan pengalaman lebih dari 5 tahun: " . implode(", ", $karyawanPengalamanLimaTahun);
+
+echo "<br><br>";
+
+$daftarNilai = [
+    'Matematika' => [
+        ['alice', 85],
+        ['bob', 92],
+        ['charlie', 78],
+    ],
+    'Fisika' => [
+        ['alice', 90],
+        ['bob', 88],
+        ['charlie', 75],
+    ],
+    'Kimia' => [
+        ['alice', 92],
+        ['bob', 80],
+        ['charlie', 85],
+    ],
+];
+
+$mataKuliah = 'Fisika';
+
+echo "Daftar nilai mahasiswa dalam mata kuliah $mataKuliah: <br>";
+
+foreach ($daftarNilai[$mataKuliah] as $nilai) {
+    echo "Nama: {$nilai[0]}, Nilai: {$nilai[1]} <br>";
+}
 ?>
